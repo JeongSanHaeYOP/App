5a06afe83d4fc8ec5561a94a16624d8be01a502f
EASYhz <jiyon3@gachon.ac.kr>
2023-01-15 00:14:49 +0900

    Update: Share Function

--- lib/resultpage.dart
+++ lib/resultpage.dart
@@ -1,13 +1,21 @@
 import 'dart:ffi';
+import 'dart:io';
+// import 'dart:html';
+import 'dart:typed_data';
 
 import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:frontend_jshy/theme/colors.dart';
 import 'package:frontend_jshy/theme/styles.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:ui' as ui;
+
+import 'package:share_plus/share_plus.dart';
 
 /// *
 /// 결과페이지
@@ -25,6 +33,8 @@ class ResultPage extends StatefulWidget {
 }
 
 class _ResultPageState extends State<ResultPage> {
+  var globalKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -41,9 +51,36 @@ class _ResultPageState extends State<ResultPage> {
             child: Container(
                 margin: const EdgeInsets.all(30),
                 child: Column(
-                  children: const [
-                    BillCard(),
-                    Text("카카오톡 공유 "),
+                  children: [
+                    RepaintBoundary(
+                      key: globalKey,
+                      child: const BillCard(),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 30, 0, 10),
+                      child: Theme(
+                          data: Theme.of(context).copyWith(
+                            splashColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            hoverColor: Colors.transparent,
+                          ),
+                          child: RawMaterialButton(
+                            onPressed: () {
+                              _capture();
+                            },
+                            elevation: 0.0,
+                            focusElevation: 0.0,
+                            fillColor: ColorStyles.mainGreen,
+                            padding: const EdgeInsets.all(15.0),
+                            shape: const CircleBorder(),
+                            child: const Icon(
+                              Icons.share,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          )),
+                    ),
+                    const Text("공유하기", style: TextStyle(fontSize: 10)),
                   ],
                 )
 
@@ -51,6 +88,48 @@ class _ResultPageState extends State<ResultPage> {
                 ),
           )),
     );
+  }
+
+  void _capture() async {
+    print("START CAPTURE");
+    var renderObject = globalKey.currentContext?.findRenderObject();
+    if (renderObject is RenderRepaintBoundary) {
+      var boundary = renderObject;
+      ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+      final directory = (await getApplicationDocumentsDirectory()).path;
+      print(directory);
+      ByteData? byteData =
+          await image.toByteData(format: ui.ImageByteFormat.png);
+      Uint8List? pngBytes = byteData?.buffer.asUint8List();
+      print(pngBytes);
+      File imgFile = File('$directory/screenshot.png');
+      print("FINISH CAPTURE ${imgFile}");
+      imgFile.writeAsBytes(pngBytes!);
+      // var imgFile = File.fromRawPath(pngBytes!);
+      if (imgFile != null) {
+        _shareBill(imgFile);
+      }
+    }
+  }
+
+  void _shareBill(File file) {
+    String text;
+    if (Get.arguments['bank'] != '') {
+      text = "정산해욥:: ${Get.arguments['bank']}";
+    } else {
+      text = "정산해욥";
+    }
+
+    if (Platform.isAndroid) {
+      print("Android");
+      Share.shareXFiles([XFile(file.path)], text: text);
+    } else {
+      print("IOS");
+      var box = globalKey.currentContext?.findRenderObject() as RenderBox?;
+      Share.shareXFiles([XFile(file.path)],
+          text: text,
+          sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size);
+    }
   }
 }
 
@@ -87,15 +166,21 @@ class _BillCardState extends State<BillCard> {
                 child: const Items(),
               ),
               dotBorder(1),
-              billText("총합계", 17, 'sum', FontWeight.normal),
+              billText("합계", 17, 'sum', FontWeight.normal),
               dotBorder(1),
               billText("총 인원", 17, 'num', FontWeight.normal),
               dotBorder(2),
-              billText("1인 지불 금액", 17, 'result', FontWeight.bold),
+              billText("1인 금액", 17, 'result', FontWeight.bold),
               Container(
                 padding: UiStyles.itemsPadding,
               ),
               bankText(),
+              Container(
+                padding: UiStyles.itemsPadding,
+              ),
+              Container(
+                padding: UiStyles.itemsPadding,
+              ),
               // 계좌 같은 추가 정보
             ],
           )
